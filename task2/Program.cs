c63f15bbaea7e2f0956f9049ea41794be49ec538
Gasteri <tipzov@yandex.ru>
2022-12-09 09:33:34 +0300

    add the sum of the digits

---
+++ task2/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Задача 27: Напишите программу, 
+которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11, 82 -> 10, 9012 -> 12 */
+
+Console.WriteLine("введите число");
+int N = InputInt();
+int InputInt()
+{
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+    if(!isParsed)
+        {
+            Console.WriteLine("Incorrect number");
+            return -1;
+        }
+    else
+        {
+            return number;
+        }
+}
+int result = GetSumOfDigits(N);
+int GetSumOfDigits(int N)
+{
+    int sum = 0;
+    while (N > 0)
+    {
+        int num = N % 10;
+        N = N / 10;
+        sum = sum + num;
+    }
+    return sum;
+}
+Console.WriteLine($"Сумма цифр в числе равна: {result}");
